c937806ec221b667b1de36e0be9d88d2e7636257
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2024-07-31 13:33:37 +0000

    Final Final Finale

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
 
-/* 
 session_start();
 
-Check if user is logged in
+//Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: ./login/login.php");
     exit();
 }
-
-*/
 
 ?>
 
