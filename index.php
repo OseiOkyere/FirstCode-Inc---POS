cd4e8feb3232e0cfe37aedfc42d951d4cc679ebb
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2024-08-02 02:22:12 +0000

    Fix login redirect to use correct file extension

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 //Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ./login/login.php");
+    header("Location: ./login/login.html");
     exit();
 }
 
